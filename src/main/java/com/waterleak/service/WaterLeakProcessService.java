2cb734f65e82bd7bde19fe5472d554f2cc5b97c7
ygleeee <yglee@ksmartpia.com>
2022-11-16 11:14:40 +0900

    패키지구조 정리 및 테스트 코드 추가

--- src/main/java/com/waterleak/service/WaterLeakProcessService.java
+++ src/main/java/com/waterleak/service/WaterLeakProcessService.java
@@ -1,8 +1,11 @@
 package com.waterleak.service;
 
 import com.waterleak.config.Globals;
+import com.waterleak.dao.reporting.AckNbiotRepository;
 import com.waterleak.dao.wapi.MtdWaterLeakExamGroupRepository;
 import com.waterleak.dao.wapi.MtdWaterLeakExamWateruserRepository;
+import com.waterleak.dto.AckNbiotDto;
+import com.waterleak.model.reporting.AckNbiot;
 import com.waterleak.model.wapi.MtdWaterLeakExamGroup;
 import com.waterleak.model.wapi.MtdWaterLeakExamWateruser;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +13,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class WapiService {
+public class WaterLeakProcessService {
+
     private final MtdWaterLeakExamGroupRepository groupRepository;
     private final MtdWaterLeakExamWateruserRepository wateruserRepository;
+    private final AckNbiotRepository ackNbiotRepository;
 
     public List<MtdWaterLeakExamWateruser> getNotYetStartExamWaterUsers(){
         List<MtdWaterLeakExamWateruser> leakExamReadyWaterUsers = new ArrayList<MtdWaterLeakExamWateruser>();
@@ -25,5 +31,13 @@ public class WapiService {
         }
         return leakExamReadyWaterUsers;
     }
+
+    public AckNbiotDto getAckNbiotBy(String imei){
+        Optional<AckNbiot> AckNbiotById = ackNbiotRepository.findById(imei);
+        if(!AckNbiotById.isPresent())
+             throw new RuntimeException("no exist imei");
+        return AckNbiotById.get().convertToDto();
+    }
+
 }
 
---
+++ src/test/java/com/waterleak/service/WaterLeakProcessServiceTest.java
@@ -0,0 +1,91 @@
+package com.waterleak.service;
+
+import com.waterleak.WaterLeak;
+import com.waterleak.config.Globals;
+import com.waterleak.dao.reporting.AckNbiotRepository;
+import com.waterleak.dao.wapi.MtdWaterLeakExamGroupRepository;
+import com.waterleak.dao.wapi.MtdWaterLeakExamWateruserRepository;
+import com.waterleak.dto.AckNbiotDto;
+import com.waterleak.model.reporting.AckNbiot;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = WaterLeak.class)
+@EnableTransactionManagement
+@ActiveProfiles("dev")
+public class WaterLeakProcessServiceTest {
+    @Autowired private MtdWaterLeakExamGroupRepository groupRepository;
+    @Autowired private MtdWaterLeakExamWateruserRepository wateruserRepository;
+    @Autowired private AckNbiotRepository ackNbiotRepository;
+    @Autowired private WaterLeakProcessService leakProcessService;
+
+    @Before
+    public void before(){
+    }
+
+    @Test
+    @Transactional(Globals.REPORTING_TRANSACTION_MANAGER)
+    public void AckNbiot_단건_조회() {
+        AckNbiot instruct = AckNbiot.builder()
+                .imei("123456789012345")
+                .nbInstruction("instruct")
+                .insertDate(Calendar.getInstance())
+                .build();
+
+        AckNbiot savedAckNbiot = ackNbiotRepository.save(instruct);
+        ackNbiotRepository.flush();
+        final Optional<AckNbiot> result = ackNbiotRepository.findById(savedAckNbiot.getImei());
+        assertTrue(result.isPresent());
+        AckNbiotDto ackNbiotBy = leakProcessService.getAckNbiotBy(instruct.getImei());
+        assertEquals(savedAckNbiot.getImei(), ackNbiotBy.getImei());
+    }
+
+    @Test
+    @Transactional(Globals.WPI_TRANSACTION_MANAGER)
+    public void 누수점검이_상태가_R_인_수용가리스트_조회_테스트() {
+    }
+
+    @Test
+    @Transactional(Globals.REPORTING_TRANSACTION_MANAGER)
+    public void 주기변경_10분_명령어_등록_테스트() {
+    }
+
+    @Test
+    @Transactional(Globals.WPI_TRANSACTION_MANAGER)
+    public void 상태가_R_인_수용가의_단말기_10분_주기변경여부확인_테스트() {
+    }
+
+    @Test
+    @Transactional(Globals.WPI_TRANSACTION_MANAGER)
+    public void 주기변경_완료후_누수점검_시작_테스트() {
+    }
+
+    @Test
+    @Transactional(Globals.WPI_TRANSACTION_MANAGER)
+    public void 누수점검_종료일이_도래한_수용가리스트_조회_및누수점검_종료_테스트() {
+    }
+
+    @Test
+    @Transactional(Globals.REPORTING_TRANSACTION_MANAGER)
+    public void 주기변경_60분_명령어_등록_테스트() {
+    }
+
+    @Test
+    @Transactional(Globals.WPI_TRANSACTION_MANAGER)
+    public void 점검이완료된_수용가들의_60분_주기변경여부확인_테스트() {
+    }
+}
