bdc8f02675969c67016d31f9c722356167392b86
ygleeee <yglee@ksmartpia.com>
2022-11-17 09:23:24 +0900

    불필요코드 제거

--- src/main/java/com/waterleak/config/Globals.java
+++ src/main/java/com/waterleak/config/Globals.java
@@ -1,8 +1,6 @@
 package com.waterleak.config;
 
 public class Globals {
-    public static final String REPORTING_TRANSACTION_MANAGER = "reportingTransactionManager";
-    public static final String WPI_TRANSACTION_MANAGER = "wapiTransactionManager";
     public static final String WATERLEAK_STATUS_READY = "R";
 }
 
--- src/test/java/com/waterleak/service/WaterLeakProcessServiceTest.java
+++ src/test/java/com/waterleak/service/WaterLeakProcessServiceTest.java
@@ -15,14 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
-import static com.waterleak.config.Globals.REPORTING_TRANSACTION_MANAGER;
-import static com.waterleak.config.Globals.WPI_TRANSACTION_MANAGER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -38,7 +35,6 @@ public class WaterLeakProcessServiceTest {
     @Autowired private WaterLeakProcessService leakProcessService;
 
     @Test
-    @Transactional(REPORTING_TRANSACTION_MANAGER)
     public void AckNbiot_단건_조회() {
         AckNbiot instruct = AckNbiot.builder()
                 .imei("890123456719876")
@@ -54,7 +50,6 @@ public class WaterLeakProcessServiceTest {
     }
 
     @Test
-    @Transactional(REPORTING_TRANSACTION_MANAGER)
     public void IMEI_활용_MeterDataSeoulNbiot_조회_테스트() {
         String targetImei = "864700040744484";
         List<MeterDataSeoulNbiot> seoulNbiots = seoulNbiotRepository.findAllByImeiOrderByMeteringDateDesc(targetImei);
@@ -63,47 +58,42 @@ public class WaterLeakProcessServiceTest {
     }
 
     @Test
-    @Transactional(REPORTING_TRANSACTION_MANAGER)
     public void 주기변경_10분_명령어_등록_테스트() {
     }
 
     @Test
-    @Transactional(WPI_TRANSACTION_MANAGER)
     public void 누수점검이_상태가_R_인_수용가리스트_조회_테스트() {
     }
 
     @Test
-    @Transactional(WPI_TRANSACTION_MANAGER)
     public void 상태가_R_인_수용가의_단말기_10분_주기변경여부확인_테스트() {
     }
 
     @Test
-    @Transactional(WPI_TRANSACTION_MANAGER)
     public void 일부_단말기의_10분_주기변경실패시_로직_테스트() {
     }
 
     @Test
-    @Transactional(WPI_TRANSACTION_MANAGER)
     public void 점검대상_단말기_주기변경_완료후_누수점검_시작_테스트() {
     }
 
     @Test
-    @Transactional(WPI_TRANSACTION_MANAGER)
     public void 누수점검_종료일이_도래한_수용가리스트_조회_및누수점검_종료_테스트() {
     }
 
     @Test
-    @Transactional(REPORTING_TRANSACTION_MANAGER)
     public void 주기변경_60분_명령어_등록_테스트() {
     }
 
     @Test
-    @Transactional(WPI_TRANSACTION_MANAGER)
     public void 일부_단말기의_60분_주기변경실패시_로직_테스트() {
     }
 
     @Test
-    @Transactional(WPI_TRANSACTION_MANAGER)
     public void 점검이완료된_수용가들의_60분_주기변경여부확인_테스트() {
+    }
+
+    @Test
+    public void 검침데이터_10분단위_를바탕으로_사용량을_계산_테스트() {
     }
 }
