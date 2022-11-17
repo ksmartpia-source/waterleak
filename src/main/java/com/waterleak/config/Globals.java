010168dda6e2ac8481172dfce5b1d482d7e2c4b4
ygleeee <yglee@ksmartpia.com>
2022-11-17 10:13:18 +0900

    누수관련 변수값 추가

--- src/main/java/com/waterleak/config/Globals.java
+++ src/main/java/com/waterleak/config/Globals.java
@@ -2,5 +2,11 @@ package com.waterleak.config;
 
 public class Globals {
     public static final String WATERLEAK_STATUS_READY = "R";
+    public static final String WATERLEAK_STATUS_START = "S";
+    public static final String WATERLEAK_STATUS_FINISH = "F";
+
+    public static final String WATERLEAK_RESULT_NORMAL = "N";
+    public static final String WATERLEAK_RESULT_LEAK = "L";
+    public static final String WATERLEAK_RESULT_FINE_LEAK = "FL";
 }
 
