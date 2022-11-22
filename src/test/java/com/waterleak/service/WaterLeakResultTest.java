656d2014f9ffb08b3a6309e06053cd19ec67a261
kny3705 <kkny3705@gmail.com>
2022-11-22 16:22:26 +0900

    누수, 미세 누수, 정상 테스트 코드 추가

--- src/test/java/com/waterleak/service/WaterLeakResultTest.java
+++ src/test/java/com/waterleak/service/WaterLeakResultTest.java
@@ -1,7 +1,11 @@
 package com.waterleak.service;
 
+import static com.waterleak.config.Globals.WATERLEAK_RESULT_FINE_LEAK;
+import static com.waterleak.config.Globals.WATERLEAK_RESULT_LEAK;
+import static com.waterleak.config.Globals.WATERLEAK_RESULT_NORMAL;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import com.waterleak.WaterLeak;
 import com.waterleak.dao.reporting.MeterDataSeoulNbiotRepository;
@@ -34,16 +38,46 @@ public class WaterLeakResultTest {
 
     @Test
     @Transactional
-    public void 누수점검_결론_도출() {
+    public void 누수점검_결론_도출_누수() {
         long examWateruserIdx = 72L;
         Optional<MtdWaterLeakExamGroup> byId = groupRepository.findById(71L);
         resultService.decision(byId.get(), examWateruserIdx);
 
         Optional<MtdWaterLeakExamWateruser> leakExamById = leakExamWateruserRepository
             .findById(examWateruserIdx);
-        assertEquals(true, leakExamById.isPresent());
+        assertTrue(leakExamById.isPresent());
         MtdWaterLeakExamWateruser finishedLeaker = leakExamById.get();
-        assertNotNull(finishedLeaker.getExamResult());
+        assertEquals(WATERLEAK_RESULT_LEAK, finishedLeaker.getExamResult());
+        assertNotNull(finishedLeaker.getLeakMinUsage());
+    }
+
+    @Test
+    @Transactional
+    public void 누수점검_결론_도출_미세누수() {
+        long examWateruserIdx = 82L;
+        Optional<MtdWaterLeakExamGroup> byId = groupRepository.findById(81L);
+        resultService.decision(byId.get(), examWateruserIdx);
+
+        Optional<MtdWaterLeakExamWateruser> leakExamById = leakExamWateruserRepository
+            .findById(examWateruserIdx);
+        assertTrue(leakExamById.isPresent());
+        MtdWaterLeakExamWateruser finishedLeaker = leakExamById.get();
+        assertEquals(WATERLEAK_RESULT_FINE_LEAK, finishedLeaker.getExamResult());
+        assertNotNull(finishedLeaker.getLeakMinUsage());
+    }
+
+    @Test
+    @Transactional
+    public void 누수점검_결론_도출_정상() {
+        long examWateruserIdx = 92L;
+        Optional<MtdWaterLeakExamGroup> byId = groupRepository.findById(91L);
+        resultService.decision(byId.get(), examWateruserIdx);
+
+        Optional<MtdWaterLeakExamWateruser> leakExamById = leakExamWateruserRepository
+            .findById(examWateruserIdx);
+        assertTrue(leakExamById.isPresent());
+        MtdWaterLeakExamWateruser finishedLeaker = leakExamById.get();
+        assertEquals(WATERLEAK_RESULT_NORMAL, finishedLeaker.getExamResult());
         assertNotNull(finishedLeaker.getLeakMinUsage());
     }
 
