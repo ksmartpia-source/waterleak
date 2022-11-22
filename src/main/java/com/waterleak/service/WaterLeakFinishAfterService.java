5d6a515eb8be82acdbb61a03d99b87a6b7363cc2
kny3705 <kkny3705@gmail.com>
2022-11-22 17:46:59 +0900

    60분 주기 변경 검증 후 명령어 삭제 서비스 구현

--- src/main/java/com/waterleak/service/WaterLeakFinishAfterService.java
+++ src/main/java/com/waterleak/service/WaterLeakFinishAfterService.java
@@ -1,9 +1,30 @@
 package com.waterleak.service;
 
+import static com.waterleak.config.Globals.CYCLE_60_MIN;
+import static com.waterleak.config.Globals.NB_INSTRUCTION_TO_60;
+
+import com.waterleak.dao.reporting.AckNbiotRepository;
+import com.waterleak.model.reporting.AckNbiot;
+import com.waterleak.utils.WaterLeakChangeVerification;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class WaterLeakFinishAfterService {
+
+    private final AckNbiotRepository ackNbiotRepository;
+    private final WaterLeakChangeVerification changeVerification;
+
+    public void restorationAfterWaterLeakExam() {
+        List<AckNbiot> ackNbiots = ackNbiotRepository.findAllByNbInstruction(NB_INSTRUCTION_TO_60);
+        for(AckNbiot ackNbiot : ackNbiots) {
+            if(changeVerification.isCycleChangeVerification(ackNbiot.getImei(), CYCLE_60_MIN)) {
+                ackNbiotRepository.deleteById(ackNbiot.getImei());
+            }
+        }
+    }
+
 }
