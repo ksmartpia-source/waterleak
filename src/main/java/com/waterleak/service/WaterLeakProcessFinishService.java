4b3dd5ab2c378ce12bc2623d5e29e96e66d072d6
ygleeee <yglee@ksmartpia.com>
2022-11-22 14:14:48 +0900

    결론도출 로직 추가

--- src/main/java/com/waterleak/service/WaterLeakProcessFinishService.java
+++ src/main/java/com/waterleak/service/WaterLeakProcessFinishService.java
@@ -7,8 +7,11 @@ import com.waterleak.dao.wapi.MtdWaterLeakExamWateruserRepository;
 import com.waterleak.model.reporting.AckNbiot;
 import com.waterleak.model.wapi.MtdWaterLeakExamGroup;
 import com.waterleak.model.wapi.MtdWaterLeakExamWateruser;
+
 import java.time.LocalDateTime;
 import java.util.List;
+
+import com.waterleak.service.result.WaterLeakResultService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,38 +19,35 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class WaterLeakProcessFinishService {
-  private final MtdWaterLeakExamGroupRepository groupRepository;
-  private final MtdWaterLeakExamWateruserRepository leakWateruserRepository;
-  private final AckNbiotRepository ackNbiotRepository;
+    private final MtdWaterLeakExamGroupRepository groupRepository;
+    private final MtdWaterLeakExamWateruserRepository leakWateruserRepository;
+    private final AckNbiotRepository ackNbiotRepository;
+    private final WaterLeakResultService resultService;
 
-  @Transactional
-  public void finishWaterLeakExam(MtdWaterLeakExamGroup group) {
-    if(isReadyToFinish(group)){
-      group.finishExam();
-      MtdWaterLeakExamGroup finishedGroup = groupRepository.save(group);
-      List<MtdWaterLeakExamWateruser> leakWaterUsers = leakWateruserRepository.findAllByExamGroup(finishedGroup);
-      for (MtdWaterLeakExamWateruser leakWaterUser : leakWaterUsers) {
-        AckNbiot to60Instruction = AckNbiot
-            .builder()
-            .imei(leakWaterUser.getImei())
-            .nbInstruction(Globals.NB_INSTRUCTION_TO_60)
-            .build();
-        ackNbiotRepository.save(to60Instruction);
-      }
-    }
-  }
 
-  public Boolean isReadyToFinish(MtdWaterLeakExamGroup group) {
-    System.out.println("############################################");
-    System.out.println("############################################");
-    System.out.println("group.getExamStatus() = " + group.getExamStatus());
-    System.out.println("############################################");
-    System.out.println("############################################");
-    LocalDateTime now = LocalDateTime.now();
-    if (now.isAfter(group.getExamFinishiedDt())
-        && group.getExamStatus().equals(Globals.WATERLEAK_STATUS_START)) {
-      return true;
+    @Transactional
+    public void finishWaterLeakExam(MtdWaterLeakExamGroup group) {
+        if (isReadyToFinish(group)) {
+            group.finishExam();
+            MtdWaterLeakExamGroup finishedGroup = groupRepository.save(group);
+            List<MtdWaterLeakExamWateruser> leakWaterUsers = leakWateruserRepository.findAllByExamGroup(finishedGroup);
+            for (MtdWaterLeakExamWateruser leakWaterUser : leakWaterUsers) {
+                AckNbiot to60Instruction = AckNbiot
+                        .builder()
+                        .imei(leakWaterUser.getImei())
+                        .nbInstruction(Globals.NB_INSTRUCTION_TO_60)
+                        .build();
+                ackNbiotRepository.save(to60Instruction);
+                resultService.decision(group, leakWaterUser.getExamWateruserIdx());
+            }
+        }
     }
-    return false;
-  }
+
+    public Boolean isReadyToFinish(MtdWaterLeakExamGroup group) {
+        if (LocalDateTime.now().isAfter(group.getExamFinishiedDt())
+                && group.getExamStatus().equals(Globals.WATERLEAK_STATUS_START)) {
+            return true;
+        }
+        return false;
+    }
 }
