2cf2ad4f931d9239866b24cdb29989472faef3a2
kny3705 <kkny3705@gmail.com>
2022-11-23 11:39:05 +0900

    60분 주기 변경 실패 테스트 코드 추가

--- src/test/java/com/waterleak/service/WaterLeakChangeCycleTest.java
+++ src/test/java/com/waterleak/service/WaterLeakChangeCycleTest.java
@@ -57,6 +57,17 @@ public class WaterLeakChangeCycleTest {
 
   @Test
   @Transactional
+  public void 주기변경_60분_검증_테스트_실패() {
+    //given
+    String tenMinIMEI = "864447050807401";
+    //when
+    Boolean result = changeVerification.isCycleChangeVerification(tenMinIMEI, Globals.CYCLE_60_MIN);
+    //then
+    assertEquals(false, result);
+  }
+
+  @Test
+  @Transactional
   public void 일부_단말기의_60분_주기변경실패시_로직_테스트() {
   }
 }
