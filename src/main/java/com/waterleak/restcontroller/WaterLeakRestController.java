941ae1ae36aed9c04baa8d674901efa367c75732
ygleeee <yglee@ksmartpia.com>
2022-11-24 17:58:41 +0900

    사용자를 위한 API 추가

--- src/main/java/com/waterleak/restcontroller/WaterLeakRestController.java
+++ src/main/java/com/waterleak/restcontroller/WaterLeakRestController.java
@@ -2,6 +2,9 @@ package com.waterleak.restcontroller;
 
 import com.waterleak.dao.reporting.AckNbiotRepository;
 import com.waterleak.dto.AckNbiotDto;
+import com.waterleak.service.WaterLeakFinishAfterService;
+import com.waterleak.service.WaterLeakProcessFinishService;
+import com.waterleak.service.WaterLeakProcessStartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,15 +21,54 @@ import static com.waterleak.config.Globals.*;
 @RestController
 public class WaterLeakRestController {
     private final AckNbiotRepository ackNbiotRepository;
+    private final WaterLeakProcessStartService startService;
+    private final WaterLeakProcessFinishService finishService;
+    private final WaterLeakFinishAfterService cleanService;
 
     @PostMapping(value = "add-instruction", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity greeting(@RequestBody AckNbiotDto ackNbiotDto) {
+    public ResponseEntity addInstruction(@RequestBody AckNbiotDto ackNbiotDto) {
         HashMap<String, Object> responseMap = new HashMap<String, Object>();
         ackNbiotRepository.save(ackNbiotDto.convertToEntity());
         responseMap.put(MESSAGE, SUCCESS_ADD_MODEM_MESSAGE);
         responseMap.put(STATUS, HttpStatus.CREATED.value());
         return ResponseEntity
                 .status(HttpStatus.CREATED)
+                .body(responseMap)
+                ;
+    }
+
+    @PostMapping(value = "start-exam")
+    public ResponseEntity startExam() {
+        HashMap<String, Object> responseMap = new HashMap<String, Object>();
+        startService.startWaterLeakExam();
+        responseMap.put(MESSAGE, SUCCESS_START_EXAM);
+        responseMap.put(STATUS, HttpStatus.OK.value());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseMap)
+                ;
+    }
+
+    @PostMapping(value = "finish-exam")
+    public ResponseEntity finishExam() {
+        HashMap<String, Object> responseMap = new HashMap<String, Object>();
+        finishService.finishWaterLeakExam();
+        responseMap.put(MESSAGE, SUCCESS_FINISH_EXAM);
+        responseMap.put(STATUS, HttpStatus.OK.value());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseMap)
+                ;
+    }
+
+    @PostMapping(value = "clean-data")
+    public ResponseEntity cleanData() {
+        HashMap<String, Object> responseMap = new HashMap<String, Object>();
+        cleanService.restorationAfterWaterLeakExam();
+        responseMap.put(MESSAGE, CLEAN_COMPLETE);
+        responseMap.put(STATUS, HttpStatus.OK.value());
+        return ResponseEntity
+                .status(HttpStatus.OK)
                 .body(responseMap)
                 ;
     }
