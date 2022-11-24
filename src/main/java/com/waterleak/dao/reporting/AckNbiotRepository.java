bf468a2a43e0c571dbd91340afe7066060522580
ygleeee <yglee@ksmartpia.com>
2022-11-24 17:32:56 +0900

    .

--- src/main/java/com/waterleak/dao/reporting/AckNbiotRepository.java
+++ src/main/java/com/waterleak/dao/reporting/AckNbiotRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AckNbiotRepository extends JpaRepository<AckNbiot, String> {
-
     List<AckNbiot> findAllByNbInstruction(String nbInstruction);
 }
