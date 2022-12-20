b49706c4ddaa0ca3d3c1108f1701b021ea6a0788
kny3705 <kkny3705@gmail.com>
2022-12-20 17:27:46 +0900

    Update WaterLeakChangeVerificationV1.java

--- src/main/java/com/waterleak/utils/WaterLeakChangeVerificationV1.java
+++ src/main/java/com/waterleak/utils/WaterLeakChangeVerificationV1.java
@@ -22,15 +22,15 @@ public class WaterLeakChangeVerificationV1 implements WaterLeakChangeVerificatio
 
   public boolean isCycleChangeVerification(String imei, Long cycle) {
     List<MeterDataSeoulNbiot> seoulNbiots = seoulNbiotRepository.findTop10ByImeiOrderByMeteringDateAsc(imei);
-    Stack<Timestamp> meteringDates = new Stack<>();
-    seoulNbiots.forEach(i -> meteringDates.push(i.getMeteringDate()));
 
-    int checkListSize = meteringDates.size();
-    List<Boolean> resultList = new ArrayList<>();
+    int checkListSize = seoulNbiots.size();
     if (checkListSize < CHECK_LIST_SIZE) {
       return false;
     }
+    Stack<Timestamp> meteringDates = new Stack<>();
+    seoulNbiots.forEach(i -> meteringDates.push(i.getMeteringDate()));
 
+    List<Boolean> resultList = new ArrayList<>();
     for (int i = 0; i < checkListSize - 1; i++) {
       long diffMin = (meteringDates.pop().getTime() - meteringDates.peek().getTime()) / 60000; //분 차이
       resultList.add(cycle == diffMin);
